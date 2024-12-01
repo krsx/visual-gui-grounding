2f2f1555a61eeff58b2a94011f319aac06559bb4
Krisna <krisnaerlangga08@gmail.com>
2024-12-01 17:55:02 +0800

    add: eval script gpt4v

--- eval_gpt4v.py
+++ eval_gpt4v.py
@@ -1,3 +1,6 @@
+import argparse
+from datetime import datetime
+import logging
 from tqdm import tqdm
 import ast
 from PIL import Image
@@ -8,50 +11,38 @@ import requests
 import json
 from dotenv import load_dotenv, find_dotenv
 
+from utils import prompt as utils_prompt
 
 load_dotenv(find_dotenv())
 
 
-results = json.load(open('./gpt4v_result_mobile.json', 'r'))
-icon_result = [item for item in results if item["data_type"] == "icon"]
-text_result = [item for item in results if item["data_type"] == "text"]
-print("icon acc: " +
-      str((len([item for item in icon_result if item["correct"]])/len(icon_result))))
-print("text acc: " +
-      str((len([item for item in text_result if item["correct"]])/len(text_result))))
-input()
-
-# OpenAI API Key
-api_key = os.getenv("OPENAI_API_KEY")
+folder_name = "logs/gpt4v/"
+if not os.path.exists(folder_name):
+    os.makedirs(folder_name)
+    print(f"Folder '{folder_name}' created successfully!")
 
 
-# Function to encode the image
-def encode_image(image_path):
-    with open(image_path, "rb") as image_file:
-        return base64.b64encode(image_file.read()).decode('utf-8')
+parser = argparse.ArgumentParser()
+parser.add_argument('--screenspot_imgs', type=str, required=True)
+parser.add_argument('--screenspot_test', type=str, required=True)
+parser.add_argument('--task', type=str, required=True)
+parser.add_argument('--max_step', type=int, default=None)
+args = parser.parse_args()
 
 
-screenspot_imgs_dir = './data/screenspot_imgs'
-test_data = json.load(open('./data/screenspot_mobile.json', 'r'))
-result = []
-num_correct = 0
-random.shuffle(test_data)
-for item in tqdm(test_data[:]):
+log_filename = folder_name + args.task + \
+    datetime.now().strftime("%Y%m%d-%H%M%S") + ".log"
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.StreamHandler(),
+        logging.FileHandler(log_filename, mode='w')
+    ]
+)
 
-    img_filename = item["img_filename"]
-    img_path = os.path.join(screenspot_imgs_dir, img_filename)
-    image = Image.open(img_path)
-    img_size = image.size
-    bbox = item["bbox"]
-    bbox = [bbox[0], bbox[1], bbox[0] + bbox[2], bbox[1] + bbox[3]]
-    bbox = [bbox[0] / img_size[0], bbox[1] / img_size[1],
-            bbox[2] / img_size[0], bbox[3] / img_size[1]]
 
-    instruction = item["instruction"]
-
-    # Getting the base64 string
-    base64_image = encode_image(img_path)
-
+def execute_llm(api_key, item, img_filename, img_path, instruction):
     headers = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {api_key}"
@@ -64,7 +55,7 @@ for item in tqdm(test_data[:]):
                    "data_type": item["data_type"], "data_souce": item["data_source"], "prompt": prompt, "correct": False}
 
     payload = {
-        "model": "gpt-4-vision-preview",
+        "model": "gpt-4o-mini",
         "messages": [
             {
                 "role": "user",
@@ -76,7 +67,7 @@ for item in tqdm(test_data[:]):
                     {
                         "type": "image_url",
                         "image_url": {
-                            "url": f"data:image/jpeg;base64,{base64_image}"
+                            "url": utils_prompt.encode_image(img_path)
                         }
                     }
                 ]
@@ -91,24 +82,145 @@ for item in tqdm(test_data[:]):
     print(response.json())
 
     result_item["response"] = response.json()
+    return result_item, response
 
-    try:
-        pred = ast.literal_eval(
-            response.json()['choices'][0]['message']['content'])
 
-        click_point = [(pred[0] + pred[2]) / 2, (pred[1] + pred[3]) / 2]
+# OpenAI API Key
+api_key = os.getenv("OPENAI_API_KEY")
+if args.task == "all":
+    tasks = ["mobile", "desktop", "web"]
+else:
+    tasks = [args.task]
+tasks_result = []
+result = []
 
-        if (bbox[0] <= click_point[0] <= bbox[2]) and (bbox[1] <= click_point[1] <= bbox[3]):
-            num_correct += 1
-            result_item["correct"] = True
-            print("correct")
-        else:
-            print("incorrect")
-    except:
-        print("wrong format")
+for task in tasks:
+    logging.info("TASK: " + task)
+    dataset = "screenspot_" + task + ".json"
+    screenspot_data = json.load(
+        open(os.path.join(args.screenspot_test, dataset), 'r'))
+    print("Num of sample: " + str(len(screenspot_data)))
 
-    result.append(result_item)
+    num_action = 0
+    corr_action = 0
+    text_correct = []
+    icon_correct = []
+    num_wrong_format = 0
 
-    json.dump(result, open('./gpt4v_result_mobile.json', 'w'))
+    for j, item in enumerate(screenspot_data):
+        if args.max_step is not None and j >= args.max_step:
+            break
+        logging.info("PROCESSING STEP: " + str(j))
 
-print("Success rate: "+str(num_correct/len(test_data)))
+        num_action += 1
+        filename = item["img_filename"]
+        img_path = os.path.join(args.screenspot_imgs, filename)
+
+        if not os.path.exists(img_path):
+            print("[ERROR] Image not found: ", img_path)
+            print("[ERROR] Please input the correct path of the image")
+            input()
+        image = Image.open(img_path)
+        instruction = item["instruction"]
+        bbox = item["bbox"]
+        bbox = [bbox[0], bbox[1], bbox[0] + bbox[2], bbox[1] + bbox[3]]
+        img_size = image.size
+        bbox = [bbox[0] / img_size[0], bbox[1] / img_size[1],
+                bbox[2] / img_size[0], bbox[3] / img_size[1]]
+
+        instruction = item["instruction"]
+
+        result_item, response = execute_llm(
+            api_key, item, filename, img_path, instruction)
+        try:
+            pred = ast.literal_eval(
+                response.json()['choices'][0]['message']['content'])
+
+            click_point = [(pred[0] + pred[2]) / 2, (pred[1] + pred[3]) / 2]
+
+            if (bbox[0] <= click_point[0] <= bbox[2]) and (bbox[1] <= click_point[1] <= bbox[3]):
+                corr_action += 1
+                result_item["correct"] = True
+                if item["data_type"] == 'text':
+                    text_correct.append(1)
+                else:
+                    icon_correct.append(1)
+                logging.info("[MATCH] " + str(corr_action / num_action))
+            else:
+                if item["data_type"] == 'text':
+                    text_correct.append(0)
+                else:
+                    icon_correct.append(0)
+                logging.info("[UNMATCH] " + str(corr_action / num_action))
+                logging.info("Agent result: " + str(click_point))
+                logging.info("Ground truth: " + str(bbox))
+            result.append(result_item)
+            logging.info("DETAILS: " + str(result[-1]))
+        except:
+            num_wrong_format += 1
+            if item["data_type"] == 'text':
+                text_correct.append(0)
+            else:
+                icon_correct.append(0)
+            logging.info("[UNMATCH] Step: " + str(j) + " wrong format!")
+            logging.info("Agent result: " + str(click_point))
+            logging.info("Ground truth: " + str(bbox))
+
+    logging.info("Action Acc: " + str(corr_action / num_action))
+    logging.info("Total num: " + str(num_action))
+    logging.info("Wrong format num: " + str(num_wrong_format))
+    logging.info("Text Acc: " + str(sum(text_correct) /
+                 len(text_correct) if len(text_correct) != 0 else 0))
+    logging.info("Icon Acc: " + str(sum(icon_correct) /
+                 len(icon_correct) if len(icon_correct) != 0 else 0))
+
+    text_acc = sum(text_correct) / \
+        len(text_correct) if len(text_correct) != 0 else 0
+    icon_acc = sum(icon_correct) / \
+        len(icon_correct) if len(icon_correct) != 0 else 0
+    tasks_result.append([text_acc, icon_acc])
+
+logging.info(tasks_result)
+
+
+# screenspot_imgs_dir = './data/screenspot_imgs'
+# test_data = json.load(open('./data/screenspot_mobile.json', 'r'))
+# result = []
+# num_correct = 0
+
+
+# for item in tqdm(test_data[:]):
+
+#     img_filename = item["img_filename"]
+#     img_path = os.path.join(screenspot_imgs_dir, img_filename)
+#     image = Image.open(img_path)
+#     img_size = image.size
+#     bbox = item["bbox"]
+#     bbox = [bbox[0], bbox[1], bbox[0] + bbox[2], bbox[1] + bbox[3]]
+#     bbox = [bbox[0] / img_size[0], bbox[1] / img_size[1],
+#             bbox[2] / img_size[0], bbox[3] / img_size[1]]
+
+#     instruction = item["instruction"]
+
+#     result_item, response = execute_llm(api_key, item, img_filename, img_path, instruction)
+
+#     try:
+#         pred = ast.literal_eval(
+#             response.json()['choices'][0]['message']['content'])
+
+#         click_point = [(pred[0] + pred[2]) / 2, (pred[1] + pred[3]) / 2]
+
+#         if (bbox[0] <= click_point[0] <= bbox[2]) and (bbox[1] <= click_point[1] <= bbox[3]):
+#             num_correct += 1
+#             result_item["correct"] = True
+#             print("correct")
+#         else:
+#             print("incorrect")
+#     except:
+#         print("wrong format")
+
+#     result.append(result_item)
+
+#     json.dump(result, open('./gpt4v_result_mobile.json', 'w'))
+
+# print("Success rate: "+str(num_correct/len(test_data)))
